e07e271d79f979335dd13019da4cf325fbc1cd11
saifiShaddil <shaddilsaifi021296@gmail.com>
2022-07-14 14:56:23 +0530

    socketio_crud_client

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import io  from 'socket.io-client'
+
+const socket = io.connect('http://127.0.0.1:4000')
+
+function App() {
+  const [count, setCount] = useState(0)
+  const handleCount = () => {
+    setCount(count + 1)
+    socket.emit('get_count', { count: count + 1})
+  }
+
+  useEffect(() => {
+    socket.on('recieve_count', (data) => {
+      console.log(data.count)
+    })
+  }, [socket])
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => handleCount()}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
