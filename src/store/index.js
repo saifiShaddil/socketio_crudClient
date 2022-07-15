68f2d2ca98763b3b8b385cf03b0974c6cba360f1
saifiShaddil <shaddilsaifi021296@gmail.com>
2022-07-15 21:56:44 +0530

    build made

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore } from "redux";
 import reducer from "./reduer";
-import { composeWithDevTools } from "redux-devtools-extension"
 
 
-export const store = createStore( reducer, composeWithDevTools());
+export const store = createStore( reducer);
